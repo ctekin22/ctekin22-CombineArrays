c3062a9eb1fb113a893d26154b791b90cf5bf215
CANSU TEKIN <ctekin22@gmail.com>
2023-08-03 19:39:52 +0000

    Arrays combined.

--- src/main/java/CombineArrays.java
+++ src/main/java/CombineArrays.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class CombineArrays {
     /**
@@ -8,7 +10,16 @@ public class CombineArrays {
      * @return an array containing the contents of arr1 followed by the contents of arr2.
      */
     public int[] combine(int[] arr1, int[] arr2){
+        int[] combined = new int[arr1.length+arr2.length];
+        
+        for(int i=0; i < arr1.length; i++){
+            combined[i] = arr1[i];
+        }
 
-        return null;
+        for(int i=0; i < arr2.length; i++){
+            combined[arr1.length+i]=arr2[i];
+        }
+
+        return combined;
     }
 }
